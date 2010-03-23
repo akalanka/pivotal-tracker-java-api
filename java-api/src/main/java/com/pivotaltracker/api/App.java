597120e8c0c36c7e7a46850b8fe621b1c7c5a323
aasplund <aasplund@gmail.com>
2010-03-23 12:12:53 +0000

    Initial commit of project structure

---
+++ java-api/src/main/java/com/pivotaltracker/api/App.java
@@ -0,0 +1,13 @@
+package com.pivotaltracker.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
