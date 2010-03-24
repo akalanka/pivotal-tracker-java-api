7782a059a08bfaebc57ffd2e1ccafa4bc20969ba
aasplund <aasplund@gmail.com>
2010-03-24 21:26:37 +0000

    Making PivotalService stateless

--- java-api/src/main/java/com/pivotaltracker/api/domain/service/PivotalService.java
+++ java-api/src/main/java/com/pivotaltracker/api/domain/service/PivotalService.java
@@ -16,13 +16,10 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.InputSource;
@@ -30,43 +27,26 @@ import org.xml.sax.InputSource;
 import com.pivotaltracker.api.domain.model.PivotalStory;
 
 /**
- * @author anders
+ * @author Anders Asplund
  * 
+ *         Since this is a java api against a RESTfull service, which is stateless, the class and its members are
+ *         stateless. Therefore it is also fully static.
  */
 public class PivotalService {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(PivotalService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PivotalService.class);
     public static final String PIVOTAL_API_URL = "http://www.pivotaltracker.com/services/v3/projects/";
     public static final String USER_TOKEN_URL = "https://www.pivotaltracker.com/services/v3/tokens/active";
 
-    private String token = "";
-    private String projectId;
-    private Credentials credentials;
-
-    public PivotalService(String token) {
-        this.token = token;
+    public static void main(String[] args) {
+        PivotalService.getUserToken("aasplund@gmail.com", "4pksfjig");
     }
-
-    public PivotalService(String token, String projectId) {
-        this.token = token;
-        this.projectId = projectId;
-    }
-
-    public PivotalService(PivotalCredentials credentials) {
-        this.credentials = new UsernamePasswordCredentials(credentials.getUserName(), credentials.getPassword());
-        setUserToken();
-    }
-
-    public PivotalService(PivotalCredentials credentials, String projectId) {
-        this.credentials = new UsernamePasswordCredentials(credentials.getUserName(), credentials.getPassword());
-        this.projectId = projectId;
-        setUserToken();
-    }
-
-    private void setUserToken() {
+    
+    public static String getUserToken(String userName, String password) {
         // Use basicAuth to make the request to the server
-        HttpResponse response = requestBasicAuthentication();
+        HttpResponse response = requestBasicAuthentication(USER_TOKEN_URL, userName, password);
         HttpEntity entity = response.getEntity();
+        String token = "";
 
         if ((response.getStatusLine().getStatusCode() == 200) && (entity.getContentLength() > 1)) {
             XPath xpath = XPathFactory.newInstance().newXPath();
@@ -75,7 +55,7 @@ public class PivotalService {
             try {
                 responseContent = response.getEntity().getContent();
                 InputSource inputSource = new InputSource(responseContent);
-                this.token = xpath.evaluate(expression, inputSource);
+                token = xpath.evaluate(expression, inputSource);
                 LOGGER.debug("Token fetched: {}", token);
             } catch (IllegalStateException e) {
                 e.printStackTrace();
@@ -85,16 +65,19 @@ public class PivotalService {
                 e.printStackTrace();
             }
         }
+        return token;
     }
 
-    private HttpResponse requestBasicAuthentication() {
+    // TODO: Move out to new class
+    private static HttpResponse requestBasicAuthentication(String url, String userName, String password) {
         DefaultHttpClient httpclient = null;
         HttpResponse response = null;
         try {
             httpclient = new DefaultHttpClient();
-            httpclient.getCredentialsProvider().setCredentials(new AuthScope(null, 443), credentials);
-            HttpGet httpget = new HttpGet(USER_TOKEN_URL);
-            LOGGER.debug("Executing request: {}",  httpget.getRequestLine());
+            httpclient.getCredentialsProvider().setCredentials(new AuthScope(null, 443),
+                    new UsernamePasswordCredentials(userName, password));
+            HttpGet httpget = new HttpGet(url);
+            LOGGER.debug("Executing request: {}", httpget.getRequestLine());
 
             response = httpclient.execute(httpget);
         } catch (ClientProtocolException e) {
@@ -109,7 +92,7 @@ public class PivotalService {
         return response;
     }
 
-    public String addStory(PivotalStory story) {
+    public static String addStory(PivotalStory story, String projectId, String token) {
         String url = PIVOTAL_API_URL + projectId + "/stories?token=" + token;
 
         String storyXml = "<story><story_type>" + story.getStoryType() + "</story_type><name>" + story.getName()
@@ -121,10 +104,10 @@ public class PivotalService {
         return addStoryOrTask(url, storyXml);
     }
 
-    public String addTask(String storyId, String description) {
+    public static String addTask(String taskDescription, String projectId, String storyId, String token) {
         String url = PIVOTAL_API_URL + projectId + "/stories/" + storyId + "/tasks?token=" + token;
 
-        String task = "<task><description>" + description + "</description></task>";
+        String task = "<task><description>" + taskDescription + "</description></task>";
 
         return addStoryOrTask(url, task);
     }
@@ -134,7 +117,7 @@ public class PivotalService {
         return "";
     }
 
-    private String addStoryOrTask(String url, String data) {
+    private static String addStoryOrTask(String url, String data) {
         return null;
         // HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
         //
@@ -197,60 +180,60 @@ public class PivotalService {
 
     public List<PivotalStory> getStories(String project) {
         return Collections.emptyList();
-//
-//        String url = PIVOTAL_API_URL + projectId + "/stories?token=" + token;
-//
-//        URL uri = new URL(url);
-//
-//        // Create a new HttpWebRequest object.
-//        HttpWebRequest request = (HttpWebRequest) WebRequest.Create(uri);
-//
-//        request.Proxy = WebProxy.GetDefaultProxy();
-//
-//        Stream objStream;
-//        objStream = request.GetResponse().GetResponseStream();
-//
-//        StreamReader objReader = new StreamReader(objStream);
-//
-//        String sLine = "";
-//        int i = 0;
-//        StringBuilder sb = new StringBuilder();
-//
-//        while (sLine != null) {
-//            i++;
-//            sLine = objReader.ReadLine();
-//            if (sLine != null) {
-//                Console.WriteLine("{0}:{1}", i, sLine);
-//                sb.Append(sLine);
-//            }
-//
-//        }
-//        Console.ReadLine();
-//
-//        String xml = sb.ToString();
-//
-//        XmlDocument xmlDoc = new XmlDocument();
-//
-//        xmlDoc.LoadXml(xml);
-//
-//        xmlDoc.Save(StoryXmlDirectory);
-//
-//        StreamReader str = new StreamReader(StoryXmlDirectory);
-//        XmlSerializer xSerializer = new XmlSerializer(typeof(PivotalStory));
-//
-//        PivotalStory myStories = (PivotalStory) xSerializer.Deserialize(str);
-//
-//        // foreach (storiesStory story in myStories.story)
-//        // {
-//        // Debug.Write(String.Format("Name: {0} {1}", story.name, Environment.NewLine));
-//        // Debug.Write(String.Format("Description: {0} {1}", story.description, Environment.NewLine));
-//        // Debug.Write(Environment.NewLine);
-//
-//        // }
-//
-//        str.Close();
-//
-//        return null;
+        //
+        // String url = PIVOTAL_API_URL + projectId + "/stories?token=" + token;
+        //
+        // URL uri = new URL(url);
+        //
+        // // Create a new HttpWebRequest object.
+        // HttpWebRequest request = (HttpWebRequest) WebRequest.Create(uri);
+        //
+        // request.Proxy = WebProxy.GetDefaultProxy();
+        //
+        // Stream objStream;
+        // objStream = request.GetResponse().GetResponseStream();
+        //
+        // StreamReader objReader = new StreamReader(objStream);
+        //
+        // String sLine = "";
+        // int i = 0;
+        // StringBuilder sb = new StringBuilder();
+        //
+        // while (sLine != null) {
+        // i++;
+        // sLine = objReader.ReadLine();
+        // if (sLine != null) {
+        // Console.WriteLine("{0}:{1}", i, sLine);
+        // sb.Append(sLine);
+        // }
+        //
+        // }
+        // Console.ReadLine();
+        //
+        // String xml = sb.ToString();
+        //
+        // XmlDocument xmlDoc = new XmlDocument();
+        //
+        // xmlDoc.LoadXml(xml);
+        //
+        // xmlDoc.Save(StoryXmlDirectory);
+        //
+        // StreamReader str = new StreamReader(StoryXmlDirectory);
+        // XmlSerializer xSerializer = new XmlSerializer(typeof(PivotalStory));
+        //
+        // PivotalStory myStories = (PivotalStory) xSerializer.Deserialize(str);
+        //
+        // // foreach (storiesStory story in myStories.story)
+        // // {
+        // // Debug.Write(String.Format("Name: {0} {1}", story.name, Environment.NewLine));
+        // // Debug.Write(String.Format("Description: {0} {1}", story.description, Environment.NewLine));
+        // // Debug.Write(Environment.NewLine);
+        //
+        // // }
+        //
+        // str.Close();
+        //
+        // return null;
 
     }
 
